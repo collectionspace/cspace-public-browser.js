a30cb5995972b1d5aefb3fd6926b0e20c1e53a8a
Caio Vinicius <caio@vncius.com>
2022-09-03 17:07:34 -0300

    Updated field name to semantic html element.

--- src/components/detail/FieldList.jsx
+++ src/components/detail/FieldList.jsx
@@ -46,7 +46,7 @@ const renderField = (id, fieldConfig, data) => {
     return (
       <React.Fragment key={id}>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <h4 {...classProp}>{title}</h4>
+        <em {...classProp}>{title}</em>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <div {...classProp}>{formattedValue}</div>
       </React.Fragment>
