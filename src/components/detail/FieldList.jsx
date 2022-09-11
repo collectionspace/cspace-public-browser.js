934bd49895430b191a2d44eb5b9f0fa0237df006
Caio Vinicius <caio@vncius.com>
2022-09-11 10:10:20 -0300

    Fixed field title html element

--- src/components/detail/FieldList.jsx
+++ src/components/detail/FieldList.jsx
@@ -46,7 +46,7 @@ const renderField = (id, fieldConfig, data) => {
     return (
       <React.Fragment key={id}>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <em {...classProp}>{title}</em>
+        <div {...classProp}>{title}</div>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <div {...classProp}>{formattedValue}</div>
       </React.Fragment>
